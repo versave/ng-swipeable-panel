0506c6cbf8deb3e51330777ad4048061eae0adf4
Versave <vszhivkov@gmail.com>
2022-04-04 19:37:23 +0300

    Remove external panel component

--- projects/ng-swipeable-panel/src/lib/ng-swipeable-panel.module.ts
+++ projects/ng-swipeable-panel/src/lib/ng-swipeable-panel.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NgSwipeablePanelComponent } from './components/ng-swipeable-panel/ng-swipeable-panel.component';
 import { CommonModule } from '@angular/common';
-import { NgSwipeablePanelExternalComponent } from './components/ng-swipeable-panel-external/ng-swipeable-panel-external.component';
-import { NgSwipeablePanelExternalContentContainerDirective } from './directives/ng-swipeable-panel-external-content-container.directive';
 import { NgSwipeablePanelTrigger } from './directives/ng-swipeable-panel-trigger.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-	declarations: [
-		NgSwipeablePanelComponent,
-		NgSwipeablePanelTrigger,
-		NgSwipeablePanelExternalComponent,
-		NgSwipeablePanelExternalContentContainerDirective,
-	],
+	declarations: [NgSwipeablePanelComponent, NgSwipeablePanelTrigger],
 	imports: [CommonModule, BrowserAnimationsModule],
-	exports: [
-		NgSwipeablePanelComponent,
-		NgSwipeablePanelTrigger,
-		NgSwipeablePanelExternalContentContainerDirective,
-	],
+	exports: [NgSwipeablePanelComponent, NgSwipeablePanelTrigger],
 })
 export class NgSwipeablePanelModule {}
--- projects/ng-swipeable-panel/src/public-api.ts
+++ projects/ng-swipeable-panel/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng-swipeable-panel
  */
 
-export * from './lib/services/ng-swipeable-panel-external.service';
 export * from './lib/components/ng-swipeable-panel/ng-swipeable-panel.component';
 export * from './lib/ng-swipeable-panel.module';
